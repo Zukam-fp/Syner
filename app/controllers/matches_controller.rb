88fc42b0ffb33904f59806ea3c756785debc0c22
marius leclercq <zigame.leclercq@gmail.com>
2024-03-07 17:38:48 +0100

    home

--- app/controllers/matches_controller.rb
+++ app/controllers/matches_controller.rb
@@ -1,5 +1,10 @@
 class MatchesController < ApplicationController
   def show
     @match = Match.find(params[:id])
+    sort_by_places
+  end
+
+  def sort_by_places
+    @match_sort = Match.all.sort_by{ |m| m.number_of_places >= 9 }.first
   end
 end
