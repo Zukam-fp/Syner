9223b8d0f235eba194e1ed8ed5ad673260bf0420
zukam fodouop <zukam.fp@outlook.com>
2024-03-14 15:21:12 +0100

    syner wip

--- app/controllers/matches_controller.rb
+++ app/controllers/matches_controller.rb
@@ -37,7 +37,7 @@ class MatchesController < ApplicationController
   end
 
   def sort_by_places
-    @match_sort = Match.all.sort_by{ |m| m.number_of_places >= 9 }.first
+    @match_sort = Match.all.sort_by{ |m| m.number_of_places }.first
   end
 
   def set_navbar_visibility
